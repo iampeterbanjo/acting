35f6233bfcda8bfbe3d74feb78c3dc8386497ec7
iampeterbanjo <peterbanjo@gmail.com>
2016-03-18 06:32:32 +0000

    Passed: ask for two actors names

--- main.go
+++ main.go
@@ -15,13 +15,25 @@ type stringReader interface {
 }
 
 // AskForName gets a name from terminal
-func AskForName(r stringReader)  {
+func AskForName(r stringReader) {
 	fmt.Println("Please enter an actor's name:")
 	name, _ := r.ReadString('\n')
 	name = strings.TrimSpace(name)
 	ActorNames = append(ActorNames, name)
 }
 
+// AskForNames will AskForName until we have 2 names
+func AskForNames(r stringReader) {
+	asking := true
+	for asking {
+		if len(ActorNames) < 2 {
+			AskForName(r)
+		} else {
+			asking = false
+		}
+	}
+}
+
 func main() {
-	AskForName(bufio.NewReader(os.Stdin))
+	AskForNames(bufio.NewReader(os.Stdin))
 }
--- main_test.go
+++ main_test.go
@@ -11,14 +11,28 @@ import (
 func Test_AskForName(t *testing.T) {
 	setup()
 
-	a := assert.New(t)
 	b := []byte("Mark\n")
 
 	r := bytes.NewBuffer(b)
 	main.AskForName(r)
 
+	a := assert.New(t)
 	a.Equal(len(main.ActorNames), 1)
 	a.Equal(main.ActorNames[0], "Mark")
+}
+
+func Test_AskForNames(t *testing.T) {
+	setup()
+
+	b := []byte("Mark\nBates\n")
+
+	r := bytes.NewBuffer(b)
+	main.AskForNames(r)
+
+	a := assert.New(t)
+	a.Equal(len(main.ActorNames), 2)
+	a.Equal(main.ActorNames[0], "Mark")
+	a.Equal(main.ActorNames[1], "Bates")
 }
 
 // tear down
