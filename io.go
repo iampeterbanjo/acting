d45121debc139741439834ec9615b4fbd57fa093
iampeterbanjo <peterbanjo@gmail.com>
2016-03-26 12:39:33 +0000

    refactor code to improve testability

---
+++ io.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+// ActorNames from terminal
+var ActorNames = []string{}
+
+type stringReader interface {
+	ReadString(byte) (string, error)
+}
+
+// AskForName gets a name from terminal
+func AskForName(r stringReader) {
+	fmt.Println("Please enter an actor's name:")
+	name, _ := r.ReadString('\n')
+	name = strings.TrimSpace(name)
+	ActorNames = append(ActorNames, name)
+}
+
+// AskForNames will AskForName until we have 2 names
+func AskForNames(r stringReader) {
+	asking := true
+	for asking {
+		if len(ActorNames) < 2 {
+			AskForName(r)
+		} else {
+			fmt.Println("Would you like to add more names? y/n")
+			answer, _ := r.ReadString('\n')
+			answer = strings.ToLower(strings.TrimSpace(answer))
+			switch answer {
+			case "y":
+				AskForName(r)
+			case "n":
+				asking = false
+			}
+		}
+	}
+}
--- main.go
+++ main.go
@@ -3,50 +3,20 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
-	"strings"
 )
 
-// ActorNames from terminal
-var ActorNames = []string{}
-
-type stringReader interface {
-	ReadString(byte) (string, error)
-}
-
-// AskForName gets a name from terminal
-func AskForName(r stringReader) {
-	fmt.Println("Please enter an actor's name:")
-	name, _ := r.ReadString('\n')
-	name = strings.TrimSpace(name)
-	ActorNames = append(ActorNames, name)
-}
-
-// AskForNames will AskForName until we have 2 names
-func AskForNames(r stringReader) {
-	asking := true
-	for asking {
-		if len(ActorNames) < 2 {
-			AskForName(r)
-		} else {
-			fmt.Println("Would you like to add more names? y/n")
-			answer, _ := r.ReadString('\n')
-			answer = strings.ToLower(strings.TrimSpace(answer))
-			switch answer {
-			case "y":
-				AskForName(r)
-			case "n":
-				asking = false
-			}
-		}
-	}
-}
-
-func main() {
+// Run user interaction
+func Run(in stringReader, out io.Writer) {
 	AskForNames(bufio.NewReader(os.Stdin))
 
 	fmt.Printf("You selected the following %d actors: \n", len(ActorNames))
 	for _, v := range ActorNames {
 		fmt.Println(v)
 	}
+}
+
+func main() {
+	Run(bufio.NewReader(os.Stdin), os.Stdout)
 }
