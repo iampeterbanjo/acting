7f2e2e235083f8fcba52dfe2717e75ad2efc9617
iampeterbanjo <peterbanjo@gmail.com>
2016-03-28 08:51:39 +0100

    add Credits struct and methods

--- credits.go
+++ credits.go
@@ -2,4 +2,25 @@ package main
 
 // Credit for Actor
 type Credit struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+	Name  string `json:"name"`
+}
+
+// NameOrTitle for TV or movie respectively
+func (c Credit) NameOrTitle() string {
+	if c.Name != "" {
+		return c.Name
+	}
+	return c.Title
+}
+
+// CreditSearchResults returns Cast credit
+type CreditSearchResults struct {
+	Cast []Credit `json:"cast"`
+}
+
+// FetchCredits for Actor
+func FetchCredits(actor *Actor) error {
+	return nil
 }
